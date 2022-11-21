33b4ce6feabad37286a13c77e51d3da1df043079
Goodguuy98 <ben.mckeever@maristdundalk.ie>
2022-11-21 13:24:40 +0000

    Fix index z-index, add button functionality

--- home-script.js
+++ home-script.js
@@ -1,10 +1,15 @@
 buttonLeft = document.querySelector("#buttonLeft");
 buttonRight = document.querySelector("#buttonRight");
 
+// Get via id cycleImg, change src
 images = [];
+// Get via id
 links = [];
 
 contentP = [];
 contentSub = [];
 contentHeading = [];
 
+function cycle(ord) {
+
+}
