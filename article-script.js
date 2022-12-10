1fd1d257438b298cec0f5806c573311d25727f22
DKITguuy9 <116917888+DKITguuy9@users.noreply.github.com>
2022-12-10 22:08:54 +0000

    Progress

---
+++ article-script.js
@@ -0,0 +1,14 @@
+https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function shareFunction() {
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(window.URL);
+
+    // Alert the copied text
+    alert("Copied the text: " + window.URL);
+}
